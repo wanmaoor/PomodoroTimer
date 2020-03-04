f70f50bb3d658790475132135a142ba35c4aed4c
wanmaoor <wanmao1284879375@outlook.com>
2020-03-04 22:18:47 +0800

    deploy

---
+++ precache-manifest.073636d516f805eec0d4b7ef290cbffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b66b73cea9e12a8a90d3d74071fb73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ea9e4f2ee0ef550c87",
+    "url": "/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "17d1bc830cb3c28814ae",
+    "url": "/static/css/main.9f2109a7.chunk.css"
+  },
+  {
+    "revision": "69ea9e4f2ee0ef550c87",
+    "url": "/static/js/2.10c8ba2d.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/static/js/2.10c8ba2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d1bc830cb3c28814ae",
+    "url": "/static/js/main.71922cc2.chunk.js"
+  },
+  {
+    "revision": "caad15334b9124e096ef",
+    "url": "/static/js/runtime-main.8450302d.js"
+  }
+]);
