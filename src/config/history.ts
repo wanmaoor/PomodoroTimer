775950337f983cd723c2676d4d4cbde4636fb54d
wanmaoor <wanmao1284879375@outlook.com>
2020-03-05 14:06:20 +0800

    优化 import

--- src/config/history.ts
+++ src/config/history.ts
@@ -1,4 +1,18 @@
-import {createBrowserHistory} from "history"
-const history = createBrowserHistory()
+import { createBrowserHistory } from "history"
+
+const ENV = process.env.NODE_ENV
+
+let publicUrl: string = ""
+
+if (ENV === "development") {
+  publicUrl = "/"
+} else if (ENV === "production") {
+  publicUrl = "/PomodoroTimer"
+}
+
+const history = createBrowserHistory({
+  basename: publicUrl
+})
+
 
 export default history
