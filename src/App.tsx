979c071b2790ea121cbe441828618581324f2997
wanmaoor <wanmao1284879375@outlook.com>
2020-03-05 17:37:29 +0800

    新增 BackToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { BackTop } from "antd"
 import React from "react"
-import "./index.css"
-import {Route, Router} from "react-router-dom"
+import { Route, Router } from "react-router-dom"
 import Home from "./components/Home/Home"
 import Login from "./components/Login/Login"
 import SignUp from "./components/SignUp/SignUp"
 import history from "./config/history"
+import "./index.css"
 
 function App() {
   return (
     <Router history={history}>
+      <BackTop/>
       <Route exact={true} path={"/"} component={Home}/>
       <Route path={"/login"} component={Login}/>
       <Route path={"/signup"} component={SignUp}/>
